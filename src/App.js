5c19642e4c9e4b3016fbe64a6919e430023c67bd
TaiLinhPN <88220193+TaiLinhPN@users.noreply.github.com>
2022-06-10 23:23:37 +0700

    newww

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Content from './components/Content';
 import Linhcom from './components/link/LinhCom';
 import LableLinh from './components/link/LableLinh';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/>
+      <Header/>
       <Content/>
-      <Footer/> */}
-      <Linhcom/>
-      <LableLinh/>
+      <Footer/>
+      {/* <Linhcom/>
+      <LableLinh/> */}
       
     </div>
   );
