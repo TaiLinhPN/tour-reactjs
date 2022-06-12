2c580c9b0258b016e697c6940b8df8263541adbf
cahoi-dev <cahoi-dev@outlook.com>
2022-06-12 22:24:26 +0700

    vu

--- src/components/Content.js
+++ src/components/Content.js
@@ -23,51 +23,12 @@ function Content() {
                 <SiteBar />
             </div>
             <div className="col c-9 row">
-
                 {tours.map(tour =>(
                     <Tour key={tour.id} img={tour.img}
                     name={tour.name}
                     price={tour.price}
                     adress={tour.adress}/>
                 ))}
-{/*                 
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" />
-
-                <Tour img="https://storage.googleapis.com/vntravel-fe/f82fa207dfb16df6831791c04ec6524b/130b6b6143e4eda9ec1567175a690b1f/dac026fdbcce80c2d3be743fd14f02c8.png"
-                    name="Tour 1 Ngày: Tham quan Phan Thiết"
-                    price="100.999"
-                    adress="Bình Thuận" /> */}
             </div>
         </div>
     )
